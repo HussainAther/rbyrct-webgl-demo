b1edfbc506500310c1eab31df6e7dac947ef97ef
Syed Hussain Ather <shussainather@gmail.com>
2026-05-03 13:19:49 -0400

    initial commit: src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LiveSimulation } from './components/LiveSimulation';
+
+const App = () => {
+  return (
+    <div style={{ width: '100vw', height: '100vh', backgroundColor: '#000' }}>
+      <LiveSimulation />
+    </div>
+  );
+};
+
+export default App;
